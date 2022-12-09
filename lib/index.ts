e01539a89542718b4d2bce50b1882615dc9cdbd6
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-09 13:23:05 +0200

    init

---
+++ lib/index.ts
@@ -0,0 +1,16 @@
+import * as composerLockFileParser from '@snyk/composer-lockfile-parser';
+
+import { systemDeps } from './system-deps';
+import { PhpPluginResult, PhpOptions } from './types';
+
+const PLUGIN_NAME = 'snyk-php-plugin';
+
+export async function inspect(basePath: string, fileName: string, options: PhpOptions = {}): Promise<PhpPluginResult> {
+  const systemVersions = systemDeps(basePath, options);
+  const depsTree = composerLockFileParser.buildDepTreeFromFiles(basePath, fileName, systemVersions, options.dev);
+
+  return Promise.resolve({
+    package: depsTree,
+    plugin: { name: PLUGIN_NAME, targetFile: fileName },
+  });
+}
